1e07b3b5e1e37bc9dbc03d668860559df3df21b0
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-09 21:46:31 +0530

    Welcome to Address Book System

---
+++ src/main/java/com/cg/addressbook/Address.java
@@ -0,0 +1,16 @@
+package com.cg.addressbook;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
+public class Address {
+
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Address Book program");
+        Scanner sc=new Scanner(System.in);
+
+    }
+
+}
