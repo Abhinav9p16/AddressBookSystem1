361da269d0327f49a1663b85897ff4c677a9f9b4
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-12 09:30:33 +0530

    Final AddBook

--- src/main/java/com/cg/addressbook/Address.java
+++ src/main/java/com/cg/addressbook/Address.java
@@ -59,23 +59,7 @@ class AddressBook {
     public List<Contact> searchState(String state){return (c.stream().filter(d->d.state.equalsIgnoreCase(state)).collect(Collectors.toList()));}
 
     public boolean nameCheck(String f, String l, int i)
-    {
-        int k=0;
-        for(i=0;i<c.size();i++)
-        {
-            if(c.get(i).first.equalsIgnoreCase(f) && c.get(i).last.equalsIgnoreCase(l))
-            {
-                k=1;
-                break;
-            }
-        }
-        if(k==0)
-        {
-            System.out.println("******No Contact found******");
-            return false;
-        }
-        return true;
-    }
+    {  return (c.get(i).first.equalsIgnoreCase(f) && c.get(i).last.equalsIgnoreCase(l)) ; }
     public void addContact()
     {
         Contact a=new Contact();
@@ -83,21 +67,14 @@ class AddressBook {
         if(checkDup(a))  System.out.println("Duplicate Contact");
         else  c.add(a);
     }
-    public void editContact(String f, String l, int i)
-    {
-        if(nameCheck(l, f, i));
-        c.get(i).editContact();
-    }
-    public void viewContact(String f, String l, int i)
-    {
-        if(nameCheck(l, f, i));
-        c.get(i).viewContact();
-    }
-    public void deleteContact(String f, String l,int i)
-    {
-        if(nameCheck(l, f, i));
-        c.remove(i);
-    }
+    public void editContact(String f, String l)
+    {   for(int i = 0; i< c.size(); i++) { if(nameCheck(f,l,i)); c.get(i).editContact(); break; }}
+
+    public void viewContact(String f, String l)
+    {   for(int i = 0; i< c.size(); i++) { if(nameCheck(f,l,i)); c.get(i).viewContact(); break; }}
+
+    public void deleteContact(String f, String l)
+    {   for(int i = 0; i< c.size(); i++) { if(nameCheck(f,l,i)); c.remove(i); break; }}
 }
 public class Address {
     public static void main(String args[])
@@ -135,33 +112,29 @@ public class Address {
             }
 
             if(x==1)
-            { a.get(name).addContact();
-
-            }
+            { a.get(name).addContact(); }
             if(x==2)
-            {   int i = 0 ;
-                System.out.println("Enter First Name");
+            {   System.out.println("Enter First Name");
                 String f=sc.next();
                 System.out.println("Enter Last Name");
                 String l=sc.next();
-                a.get(name).editContact(f, l, i);
+                a.get(name).editContact(f, l);
             }
             if(x==3)
-            {   int i = 0 ;
+            {
                 System.out.println("Enter First Name");
                 String f=sc.next();
                 System.out.println("Enter Last Name");
                 String l=sc.next();
-                a.get(name).viewContact(f, l , i);
-
+                a.get(name).viewContact(f, l);
             }
             if(x==4)
-            {   int i = 0;
+            {
                 System.out.println("Enter First Name");
                 String f=sc.next();
                 System.out.println("Enter Last Name");
                 String l=sc.next();
-                a.get(name).deleteContact(f, l, i);
+                a.get(name).deleteContact(f, l);
 
             }
             if(x==6)
@@ -189,7 +162,7 @@ public class Address {
 
                 List<Contact> b = new ArrayList<>();
                 for( Map.Entry<String,AddressBook> i :a.entrySet()) {
-                    b.addAll(i.getValue().searchCity(state));
+                    b.addAll(i.getValue().searchState(state));
                 }
                 if(b.size() == 0)
                 { System.out.println("No contacts");}
